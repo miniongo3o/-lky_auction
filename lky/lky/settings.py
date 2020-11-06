46a3f2a7f8b60dceed043d1a53b8a58391852e54
Eunseo Yu <youesdia0704@gmail.com>
2020-11-06 18:08:11 +0900

    product detail - countdown

--- lky/lky/settings.py
+++ lky/lky/settings.py
@@ -135,5 +135,10 @@ USE_TZ = False
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, STATIC_URL)
 
+<<<<<<< Updated upstream
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+=======
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, MEDIA_URL)
+>>>>>>> Stashed changes
